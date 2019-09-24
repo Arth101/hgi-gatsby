c43a822a765092d0f850bb4d29271c6d369b88e5
Anders Enghoej <andersenghoej@gmail.com>
2019-09-24 14:57:57 +0200

    change logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/hgi-logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
